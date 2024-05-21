1395a6efc9482e033a1a93d942ca40f748adc643
MinHyup1 <p01073027511@gmail.com>
2024-05-21 22:13:34 +0900

    [refactor] 문자열 디폴트 구분자 기준으로 분리한 숫자를 모두 더할 수 있다.

--- src/main/java/com/stringcalculator/StringCalculator.java
+++ src/main/java/com/stringcalculator/StringCalculator.java
@@ -2,16 +2,28 @@ package com.stringcalculator;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class StringCalculator {
+
+    private final static String DEFAULT_SEPARATOR = "[,:]";
 
     // default 구분자 쉼표(,) , 콜론(:)
     // while 문을 돌며 구분자가 존재하는지 확인하고
 
     public int plusStringNumbersBySeparator(String value) {
-        List<Integer> integers = splitIntegerBySeparator(value);
+        // 패턴 컴파일
+        Pattern compiledPattern = Pattern.compile(DEFAULT_SEPARATOR);
 
-        return pushIntegerList(integers);
+        // 입력 문자열에 패턴 매칭
+        Matcher matcher = compiledPattern.matcher(value);
+
+        if(matcher.find()) {
+            return pushIntegerList(splitIntegerByDefaultSeparator(value));
+        }
+
+        throw new RuntimeException();
     }
 
     private int pushIntegerList(List<Integer> integers) {
@@ -21,10 +33,9 @@ public class StringCalculator {
         }
         return result;
     }
-
-    private List<Integer> splitIntegerBySeparator(String value) {
+    private List<Integer> splitIntegerByDefaultSeparator(String value) {
         List<Integer> result = new ArrayList<>();
-        for (String s : value.split("[,:]")) {
+        for (String s : value.split(DEFAULT_SEPARATOR)) {
             result.add(Integer.parseInt(s));
         }
         return result;
