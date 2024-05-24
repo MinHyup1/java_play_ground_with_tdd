2b24c1177b77a3bb00c4e53fbf9d26ee1f615335
MinHyup1 <p01073027511@gmail.com>
2024-05-25 00:40:28 +0900

    [feat] 자동차 일급컬렉션 이름 유효성 테스트 추가

--- src/main/java/com/racingcar/domain/Cars.java
+++ src/main/java/com/racingcar/domain/Cars.java
@@ -1,5 +1,7 @@
 package com.racingcar.domain;
 
+import com.racingcar.validation.Validation;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -7,7 +9,10 @@ public class Cars {
 
     private final List<Car> carList = new ArrayList<Car>();
     public Cars(String carNames) {
+        Validation validation = new Validation();
+
         for (String carName : carNames.split(",")) {
+            validation.carNameValidation(carName);
             this.carList.add(new Car(carName));
         }
     }
