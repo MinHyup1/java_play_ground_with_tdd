32fb7da439e2a05ef9436b2f90a73bfe76bf2f0e
MinHyup1 <p01073027511@gmail.com>
2024-05-25 21:26:26 +0900

    [feat] 자동차의 위치값을 랜덤으로 4이상 9이하의 값으로 전진 테스트

--- src/main/java/com/racingcar/domain/Car.java
+++ src/main/java/com/racingcar/domain/Car.java
@@ -1,11 +1,27 @@
 package com.racingcar.domain;
 
 import java.util.Objects;
+import java.util.Random;
 
 public class Car {
     private final String carName;
+    private int location;
+
     public Car(String carName) {
         this.carName = carName;
+    }
+
+    public int getLocation() {
+        return this.location;
+    }
+
+    public void moveForward() {
+        this.location += create4to9RandomNumber();
+    }
+
+    private int create4to9RandomNumber() {
+        Random random = new Random();
+        return random.nextInt(6) + 4;
     }
 
     @Override
--- src/test/java/com/racingcar/CarTest.java
+++ src/test/java/com/racingcar/CarTest.java
@@ -12,15 +12,24 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class CarTest {
 
 
-    // 이런것 까지 TDD로 해야하는지 싶다
+    // 이런것 까지 TDD 로 해야하는지 싶다
 
     @Test
     @DisplayName("자동차이름이 , 로 구분된 자동차 일급 컬렉션을 생성할 수 있다.")
     void 자동차이름_목록_문자열을_일급컬렉션으로_생성할_수_있다() {
         String carNames = "one,two,three,four";
         Cars cars = new Cars(carNames);
-        List<Car>  carList = cars.getCarList();
+        List<Car> carList = cars.getCarList();
         assertThat(carList).contains(new Car("one"), new Car("two"), new Car("three"), new Car("four"));
     }
-    // 위치값이 가장 큰 값이 우승이다.(우승지는 한명이상 일 수 있다)
+
+    @Test
+    @DisplayName("자동차의 위치값을 랜덤으로 4이상 9이하의 값으로 전진할 수 있다.")
+    void 자동차의_위차값을_전진할_수_있다() {
+        Car car = new Car("car");
+        int carLocationBeforeMove = car.getLocation();
+        car.moveForward();
+        int carLocationAfterMove = car.getLocation();
+        assertThat(carLocationBeforeMove).isLessThan(carLocationAfterMove);
+    }
 }
