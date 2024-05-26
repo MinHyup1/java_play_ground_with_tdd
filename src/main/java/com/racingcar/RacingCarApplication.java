86decce2113ce53d9d3e1b4c3dae3ba4d8334e52
MinHyup1 <p01073027511@gmail.com>
2024-05-26 12:01:22 +0900

    [feat] 자동차 경주 게임 UI 생성, 및 메인 application 구현 완료

---
+++ src/main/java/com/racingcar/RacingCarApplication.java
@@ -0,0 +1,17 @@
+package com.racingcar;
+
+import com.racingcar.domain.Cars;
+import com.racingcar.ui.InputView;
+import com.racingcar.ui.ResultView;
+
+public class RacingCarApplication {
+    public static void main(String[] args) {
+        InputView inputView = new InputView();
+        String inputValue = inputView.startInputPrompt();
+        Cars cars = new Cars(inputValue);
+        int times = inputView.startTryTimesPrompt();
+
+        ResultView resultView = new ResultView();
+        resultView.printResults(cars, times);
+    }
+}
