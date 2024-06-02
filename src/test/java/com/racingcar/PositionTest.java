d68d3b6d64354ca3c30f215367459d4d646ee966
Jaehong Choe <qnalsfj@gmail.com>
2024-06-02 18:57:58 +0900

    feat Position 인스턴스간 위치 비교 기능 추가

--- src/test/java/com/racingcar/PositionTest.java
+++ src/test/java/com/racingcar/PositionTest.java
@@ -1,6 +1,7 @@
 package com.racingcar;
 
-import org.assertj.core.api.Assertions;
+import static org.assertj.core.api.Assertions.*;
+
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
@@ -11,6 +12,22 @@ public class PositionTest {
     @Test
     void Position은_위치값을_주입받아_생성된다() {
         Position position = new Position(3);
-        Assertions.assertThat(position).isNotNull();
+        assertThat(position).isNotNull();
+    }
+
+    @Test
+    void Position은_Position_인스턴스간_위치값_비교가_가능하다() {
+        Position one = new Position(0);
+        Position theOther = new Position(5);
+
+        assertThat(one.isLessThan(theOther)).isTrue();
+    }
+
+    @Test
+    void Position은_Position_인스턴스간_위치값_비교시_null을_입력받으면_NPE를_발생시킨다() {
+        Position one = new Position(0);
+
+        assertThatThrownBy(() -> one.isLessThan(null))
+            .isInstanceOf(NullPointerException.class);
     }
 }
