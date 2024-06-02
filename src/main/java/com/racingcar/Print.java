c3a456aac55e37fcf5bfd105d09710c61beaedfa
Jaehong Choe <qnalsfj@gmail.com>
2024-06-02 20:54:18 +0900

    feat 자동차 위치 출력 기능 추가

--- src/main/java/com/racingcar/Print.java
+++ src/main/java/com/racingcar/Print.java
@@ -8,10 +8,24 @@ public class Print {
     private static final String WINNER_MESSAGE = "가 최종 우승했습니다.";
     private static final String WINNER_DELIMITER = ", ";
 
+    private static final String POSITION_DELIMITER = " : ";
+    private static final String POSITION_BAR = "-";
+
     public static void winners(List<String> winners) {
         String winnerStr = winners.stream()
             .collect(Collectors.joining(WINNER_DELIMITER));
 
         System.out.println(winnerStr + WINNER_MESSAGE);
+    }
+
+    public static void positions(List<CarPosition> positions) {
+        for (CarPosition position : positions) {
+            String positionBars = POSITION_BAR.repeat(position.getPosition());
+            StringBuilder sb = new StringBuilder()
+                .append(position.getName()).append(POSITION_DELIMITER).append(positionBars);
+            System.out.println(sb.toString());
+        }
+
+        System.out.println();
     }
 }
--- src/test/java/com/racingcar/PrintTest.java
+++ src/test/java/com/racingcar/PrintTest.java
@@ -9,4 +9,16 @@ public class PrintTest {
     void test() {
         Print.winners(List.of("자동차A", "자동차B"));
     }
+
+    @Test
+    void positions() {
+        Cars cars = new Cars(List.of(
+            new Car("자동차1", 3),
+            new Car("자동차2", 2),
+            new Car("자동차3", 4)));
+
+        List<CarPosition> positions = cars.getCurrentPositions();
+
+        Print.positions(positions);
+    }
 }
