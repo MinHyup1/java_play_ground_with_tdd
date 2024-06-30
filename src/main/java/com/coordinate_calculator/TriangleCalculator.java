ad86372f5682bdf233819314c28669afd69363ce
Jaehong Choe <qnalsfj@gmail.com>
2024-06-30 21:00:49 +0900

    test 삼각형 계산기 검산 테스트 추가

--- src/main/java/com/coordinate_calculator/TriangleCalculator.java
+++ src/main/java/com/coordinate_calculator/TriangleCalculator.java
@@ -16,7 +16,16 @@ public class TriangleCalculator implements FigureCalculable {
 
     @Override
     public Figure calculate() {
-        Double area = null;//todo 계산
+        Coordinate one = coordinates.get(0);
+        Coordinate two = coordinates.get(1);
+        Coordinate three = coordinates.get(2);
+
+        // Line todo 현재 Line은 Figure를 구현한 출력담당 클래스이지만 도형 계산에 도움을 줄 Line 객체가 있으면  좋을 것 같다.
+        Double a = one.calculateDistance(two);
+        Double b = two.calculateDistance(three);
+        Double c = three.calculateDistance(one);
+
+        double area = Math.sqrt((a + b + c) * (- a + b + c) * (a - b + c) * (a + b - c)) / 4;
         return new Triangle(coordinates, area);
     }
 }
