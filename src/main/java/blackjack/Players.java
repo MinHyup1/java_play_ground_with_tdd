461a61fe9c90b0a79ac868af6cdd6d68b5ede0fe
Jaehong Choe <qnalsfj@gmail.com>
2024-08-04 19:27:19 +0900

    feat Players 생성자 인자 변경
    - Players 내부에서 Player들의 카드 추가 뽑기 여부를 상태로 관리할 생각이라서
    처음부터 외부에서 주입하는 방식을 선택.

--- src/main/java/blackjack/Players.java
+++ src/main/java/blackjack/Players.java
@@ -2,13 +2,19 @@ package blackjack;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 public class Players {
 
-    private List<Player> players;
+    private List<PlayerStatus> playerStatuses;
 
-    Players(List<Player> players) {
-        this.players = players;
+    Players(List<PlayerStatus> playerStatuses) {
+        this.playerStatuses = new ArrayList<>(playerStatuses);
+        addDealerStatus();
+    }
+    //todo 딜러를 구분하고 싶은데..
+    private void addDealerStatus() {
+        Player dealer = Player.dealer(new CardDeck());
+        PlayerStatus dealerStatus = new PlayerStatus(dealer);
+        this.playerStatuses.add(dealerStatus);
     }
 }
--- src/test/java/blackjack/PlayersTest.java
+++ src/test/java/blackjack/PlayersTest.java
@@ -8,14 +8,16 @@ import org.junit.jupiter.api.Test;
 @DisplayName("Players 단위테스트")
 public class PlayersTest {
 
-    @DisplayName("Players는 Player 목록을 입력하여 생성한다.")
+    @DisplayName("Players는 PlayerStatus 목록을 입력하여 생성한다.")
     @Test
-    void players_created_with_participants_names_and_their_bet_amounts() {
-        Player dealer = Player.dealer(new CardDeck());
+    void players_created_with_player_statuses() {
         Player 참가자1 = Player.participant("참가자1", 10000, new CardDeck());
         Player 참가자2 = Player.participant("참가자2", 20000, new CardDeck());
         Player 참가자3 = Player.participant("참가자3", 30000, new CardDeck());
-        Players players = new Players(List.of(dealer, 참가자1, 참가자2, 참가자3));
+        Players players = new Players(List.of(
+            new PlayerStatus(참가자1),
+            new PlayerStatus(참가자2),
+            new PlayerStatus(참가자3)));
 
         Assertions.assertThat(players).isNotNull();
     }
